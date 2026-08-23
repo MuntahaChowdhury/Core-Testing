3779e535c07efb1c38e9e58a92d008e7537e2474
MuntahaChowdhury <muntaha0108@gmail.com>
2026-08-23 00:25:36 -0400

    Updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -14,25 +14,35 @@ export default function Home() {
 
 
   return (
-    <div className="p-20 w-full min-h-screen bg-gradient-to-l from-bru2 to-bru4 text-center">
+    <div className="relative p-20 w-full min-h-screen bg-gradient-to-b from-bru1 to-bru2">
 
-      <h1 className="font-extrabold text-4xl tracking-tight"> Core Service Tester Project</h1>
-      <p className="mt-2 text-lg text-yellow-200">Hello {checker}</p>
+      <div className="absolute top-0 left-0 bg-bru1 border-b-2 border-bru5 w-full p-2 text-center"><p className="font-extrabold text-xs text-bru5 tracking-tight"> Core Service Tester Project</p></div>
 
+      <section>
+        <h1 className="font-extrabold text-4xl text-bru5 tracking-tight">BuyerPanda</h1>
+        <div className="grid grid-cols-2 space-x-8">
 
-      <div className="grid grid-cols-4 grid-rows-auto gap-6 mt-10 mx-[15vw] text-white">
-        <button className="homeButton" onClick={() => window.location.href = "/otp"}>OTP</button>
-        <button className="homeButton" onClick={() => window.location.href = "/s3"}>s3</button>
-        <button className="homeButton" onClick={() => window.location.href = "/user/login"}>User/Login</button>
-        <button className="homeButton" onClick={() => window.location.href = "/user/logout"}>User/Logout</button>
-        <button className="homeButton" onClick={() => window.location.href = "/user/signup"}>User/SignUp</button>
-        <button className="homeButton" onClick={() => window.location.href = "/user/menu"}>User/Menu</button>
-        <button className="homeButton" onClick={() => window.location.href = "/user/profile"}>User/Profile</button>
-        <button className="homeButton" onClick={() => window.location.href = "/user/cfpwd"}>User/Password</button>
-        <button className="homeButton" onClick={() => window.location.href = "/user/dboard"}>User/Dashboard</button>
-        <button className="homeButton" onClick={() => window.location.href = "/hp"}>Hero Section</button>
-      </div>
+          <div className="grid grid-cols-4 grid-rows-auto gap-4 mt-4 w-fit text-white">
+            <button className="homeButton" onClick={() => window.location.href = "/otp"}>OTP</button>
+            <button className="homeButton" onClick={() => window.location.href = "/s3"}>s3</button>
+            <button className="homeButton" onClick={() => window.location.href = "/user/login"}>User/Login</button>
+            <button className="homeButton" onClick={() => window.location.href = "/user/logout"}>User/Logout</button>
+            <button className="homeButton" onClick={() => window.location.href = "/user/signup"}>User/SignUp</button>
+            <button className="homeButton" onClick={() => window.location.href = "/user/menu"}>User/Menu</button>
+            <button className="homeButton" onClick={() => window.location.href = "/user/profile"}>User/Profile</button>
+            <button className="homeButton" onClick={() => window.location.href = "/user/cfpwd"}>User/Password</button>
+            <button className="homeButton" onClick={() => window.location.href = "/user/dboard"}>User/Dashboard</button>
+            <button className="homeButton" onClick={() => window.location.href = "/hp"}>Hero Section</button>
+          </div>
 
+          <div className="testOutput">
+            <h1 className="font-extrabold text-md text-bru3 tracking-tight">Output:</h1>
+            <p className="mt-2 text-lg text-bru4 w-full">Hello {checker}</p>
+          </div>
+
+        </div>
+      </section>
+      
     </div>
   );
 }
